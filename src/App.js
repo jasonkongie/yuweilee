3acd182fc10280317f4ee938ce4e3acc5bf12626
jasonkongie <68826525+jasonkongie@users.noreply.github.com>
2024-02-28 14:23:00 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { useEffect } from 'react';
 function App() {
 
   useEffect(() => {
-    window.location.href = 'https://youtu.be/YWetsCFFEE8';
+    window.location.href = 'https://www.oberlin.edu/events/junior_recital_jamie_phillips-freedman_and_yu-wei_lee_piano?fbclid=PAAabLOpBQqoKVnS9jMHQJyltltzxC2ToYLfcLS2MKFMoECWVVmUbIXNb-5kk_aem_AYgLfOKp2wYU_tTCHuksFmRo5KvDuvrw5xFokFZ--KaPpqB4QU2Ku5eNBr4lbjQf0rc';
   }, []);
 
   return (
